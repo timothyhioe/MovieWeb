b8bba83e27f0777448be66129220885509d82ceb
Timothy <timothy.hioe@stud.h-da.de>
2025-04-20 21:16:51 +0200

    Deploy ready

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/MovieWeb/'
+  base: '/MovieWeb/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '', // Copy to dist/ as 404.html
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
 })
